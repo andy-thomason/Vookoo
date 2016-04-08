3f1dfa33ea615b462241d07d65d0ac75019b79fe
Andy Thomson <andy@andythomason.com>
2016-04-08 16:49:50 +0100

    swapchain cleanup

--- base/vulkanswapchain.hpp
+++ base/vulkanswapchain.hpp
@@ -74,15 +74,6 @@ public:
 	VkFormat colorFormat = VK_FORMAT_B8G8R8A8_UNORM;;
 	VkColorSpaceKHR colorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
 
-
-  /*
-	VkImage* swapchainImages;
-
-	//VkSwapchainKHR swapChain = VK_NULL_HANDLE;
-
-	uint32_t imageCount;
-	SwapChainBuffer* buffers;*/
-
 	// Index of the deteced graphics and presenting device queue
 	uint32_t queueNodeIndex = UINT32_MAX;
 
@@ -239,111 +230,9 @@ public:
 
 	void setup(VkCommandBuffer cmdBuffer, uint32_t *width, uint32_t *height)
 	{
-		//VkResult err;
-		/*VkSwapchainKHR oldSwapchain = swapChain;
-
-		// Get physical device surface properties and formats
-		VkSurfaceCapabilitiesKHR surfCaps;
-		err = fpGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfCaps);
-		assert(!err);
-
-		uint32_t presentModeCount;
-		err = fpGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, NULL);
-		assert(!err);
-
-		// todo : replace with vector?
-		VkPresentModeKHR *presentModes = (VkPresentModeKHR *)malloc(presentModeCount * sizeof(VkPresentModeKHR));
-
-		err = fpGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, presentModes);
-		assert(!err);
-
-		VkExtent2D swapchainExtent = {};
-		// width and height are either both -1, or both not -1.
-		if (surfCaps.currentExtent.width == -1)
-		{
-			// If the surface size is undefined, the size is set to
-			// the size of the images requested.
-			swapchainExtent.width = *width;
-			swapchainExtent.height = *height;
-		}
-		else
-		{
-			// If the surface size is defined, the swap chain size must match
-			swapchainExtent = surfCaps.currentExtent;
-			*width = surfCaps.currentExtent.width;
-			*height = surfCaps.currentExtent.height;
-		}
-
-		// Try to use mailbox mode
-		// Low latency and non-tearing
-		VkPresentModeKHR swapchainPresentMode = VK_PRESENT_MODE_FIFO_KHR;
-		for (size_t i = 0; i < presentModeCount; i++) 
-		{
-			if (presentModes[i] == VK_PRESENT_MODE_MAILBOX_KHR) 
-			{
-				swapchainPresentMode = VK_PRESENT_MODE_MAILBOX_KHR;
-				break;
-			}
-			if ((swapchainPresentMode != VK_PRESENT_MODE_MAILBOX_KHR) && (presentModes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR)) 
-			{
-				swapchainPresentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
-			}
-		}
-
-		// Determine the number of images
-		uint32_t desiredNumberOfSwapchainImages = surfCaps.minImageCount + 1;
-		if ((surfCaps.maxImageCount > 0) && (desiredNumberOfSwapchainImages > surfCaps.maxImageCount))
-		{
-			desiredNumberOfSwapchainImages = surfCaps.maxImageCount;
-		}
-
-		VkSurfaceTransformFlagsKHR preTransform;
-		if (surfCaps.supportedTransforms & VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR)
-		{
-			preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
-		}
-		else {
-			preTransform = surfCaps.currentTransform;
-		}
-
-		VkSwapchainCreateInfoKHR swapchainCI = {};
-		swapchainCI.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-		swapchainCI.pNext = NULL;
-		swapchainCI.surface = surface;
-		swapchainCI.minImageCount = desiredNumberOfSwapchainImages;
-		swapchainCI.imageFormat = colorFormat;
-		swapchainCI.imageColorSpace = colorSpace;
-		swapchainCI.imageExtent = { swapchainExtent.width, swapchainExtent.height };
-		swapchainCI.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
-		swapchainCI.preTransform = (VkSurfaceTransformFlagBitsKHR)preTransform;
-		swapchainCI.imageArrayLayers = 1;
-		swapchainCI.queueFamilyIndexCount = VK_SHARING_MODE_EXCLUSIVE;
-		swapchainCI.queueFamilyIndexCount = 0;
-		swapchainCI.pQueueFamilyIndices = NULL;
-		swapchainCI.presentMode = swapchainPresentMode;
-		swapchainCI.oldSwapchain = oldSwapchain;
-		swapchainCI.clipped = true;
-		swapchainCI.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-
-    VkSwapchainKHR swapChain2;
-		err = fpCreateSwapchainKHR(device, &swapchainCI, nullptr, &swapChain2);
-		assert(!err);
-
-    swapChain = vku::swapChain(swapChain2);*/
-
     swapChain = vku::swapChain(vku::device(device, physicalDevice), *width, *height, surface, cmdBuffer);
     *width = swapChain.width();
     *height = swapChain.height();
-
-		// If we just re-created an existing swapchain, we should destroy the old
-		// swapchain at this point.
-		// Note: destroying the swapchain also cleans up all its associated
-		// presentable images once the platform is done with them.
-		/*if (oldSwapchain != VK_NULL_HANDLE) 
-		{ 
-			fpDestroySwapchainKHR(device, oldSwapchain, nullptr);
-		}*/
-
 	}
 
 	// Acquires the next image in the swap chain
@@ -361,11 +250,6 @@ public:
 
 	void cleanup()
 	{
-		/*for (uint32_t i = 0; i < this->; i++)
-		{
-			vkDestroyImageView(device, buffers[i].view, nullptr);
-		}*/
-		//fpDestroySwapchainKHR(device, swapChain, nullptr);
     swapChain.clear();
 		vkDestroySurfaceKHR(instance, surface, nullptr);
 	}
