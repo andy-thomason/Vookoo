c34cdf9cc6035f626f065fbdc3131b93568d5712
Andy Thomson <andy@andythomason.com>
2017-04-20 15:42:18 +0100

    better shadow example

--- examples/teapot.cpp
+++ examples/teapot.cpp
@@ -92,13 +92,19 @@ int main() {
   lightToWorld = glm::rotate(lightToWorld, glm::radians(90.0f), glm::vec3(0, 1, 0));
   lightToWorld = glm::rotate(lightToWorld, glm::radians(-30.0f), glm::vec3(1, 0, 0));
 
+  // This matrix converts between OpenGL perspective and Vulkan perspective.
+  // It flips the Y axis and shrinks the Z value to [0,1]
   glm::mat4 leftHandCorrection(
     1.0f,  0.0f, 0.0f, 0.0f,
     0.0f, -1.0f, 0.0f, 0.0f,
     0.0f,  0.0f, 0.5f, 0.0f,
-    0.0f,  0.0f, 0.5f, 1.0f);
+    0.0f,  0.0f, 0.5f, 1.0f
+  );
+
+  uint32_t shadowSize = 512;
+
   glm::mat4 cameraToPerspective = leftHandCorrection * glm::perspective(glm::radians(45.0f), (float)window.width()/window.height(), 1.0f, 100.0f);
-  glm::mat4 lightToPerspective = leftHandCorrection * glm::perspective(glm::radians(45.0f), (float)256/256, 1.0f, 100.0f);
+  glm::mat4 lightToPerspective = leftHandCorrection * glm::perspective(glm::radians(30.0f), (float)shadowSize/shadowSize, 1.0f, 100.0f);
 
   bool lookFromLight = false;
   if (lookFromLight) {
@@ -137,12 +143,11 @@ int main() {
   ////////////////////////////////////////
   //
   // Build a pipeline for shadows
-  uint32_t shadow_size = 256;
 
   vku::ShaderModule shadow_vert{device, BINARY_DIR "teapot.shadow.vert.spv"};
   vku::ShaderModule shadow_frag{device, BINARY_DIR "teapot.shadow.frag.spv"};
 
-  vku::PipelineMaker spm{shadow_size, shadow_size};
+  vku::PipelineMaker spm{shadowSize, shadowSize};
   spm.shader(vk::ShaderStageFlagBits::eVertex, shadow_vert);
   spm.shader(vk::ShaderStageFlagBits::eFragment, shadow_frag);
   spm.vertexBinding(0, (uint32_t)sizeof(Vertex));
@@ -153,12 +158,14 @@ int main() {
   spm.cullMode(vk::CullModeFlagBits::eBack);
   spm.frontFace(vk::FrontFace::eClockwise);
 
-  vku::DepthStencilImage shadowImage(device, fw.memprops(), shadow_size, shadow_size);
+  vku::DepthStencilImage shadowImage(device, fw.memprops(), shadowSize, shadowSize);
 
   // Build the renderpass using only depth/stencil.
   vku::RenderpassMaker rpm;
 
   // The depth/stencil attachment.
+  // Clear to 1.0f at the start (eClear)
+  // Save the depth buffer at the end. (eStore)
   rpm.attachmentBegin(shadowImage.format());
   rpm.attachmentLoadOp(vk::AttachmentLoadOp::eClear);
   rpm.attachmentStoreOp(vk::AttachmentStoreOp::eStore);
@@ -168,27 +175,37 @@ int main() {
   rpm.subpassBegin(vk::PipelineBindPoint::eGraphics);
   rpm.subpassDepthStencilAttachment(vk::ImageLayout::eDepthStencilAttachmentOptimal, 0);
 
-  // A dependency to reset the layout of both attachments.
+  // A dependency to reset the layout of the depth buffer to eUndefined (the default for initial layout)
+  // eLateFragmentTests is the stage where the Z buffer is written.
   rpm.dependencyBegin(VK_SUBPASS_EXTERNAL, 0);
-  rpm.dependencySrcStageMask(vk::PipelineStageFlagBits::eEarlyFragmentTests);
+  rpm.dependencySrcStageMask(vk::PipelineStageFlagBits::eBottomOfPipe);
   rpm.dependencyDstStageMask(vk::PipelineStageFlagBits::eLateFragmentTests);
 
-  // Use the maker object to construct the vulkan object
+  // A dependency to transition to eShaderReadOnlyOptimal
+  rpm.dependencyBegin(0, VK_SUBPASS_EXTERNAL);
+  rpm.dependencySrcStageMask(vk::PipelineStageFlagBits::eLateFragmentTests);
+  rpm.dependencyDstStageMask(vk::PipelineStageFlagBits::eBottomOfPipe);
+
+  // Use the maker object to construct the renderpass
   vk::UniqueRenderPass shadowRenderPass = rpm.createUnique(device);
 
+  // Build the framebuffer.
   vk::ImageView attachments[1] = {shadowImage.imageView()};
-  vk::FramebufferCreateInfo fbci{{}, *shadowRenderPass, 1, attachments, shadow_size, shadow_size, 1 };
+  vk::FramebufferCreateInfo fbci{{}, *shadowRenderPass, 1, attachments, shadowSize, shadowSize, 1 };
   vk::UniqueFramebuffer shadowFrameBuffer = device.createFramebufferUnique(fbci);
 
+  // Build the pipeline for this renderpass.
   auto shadowPipeline = spm.createUnique(device, cache, *pipelineLayout, *shadowRenderPass, false);
 
+  // Only one attachment to clear, the depth buffer.
   vk::ClearDepthStencilValue clearDepthValue{ 1.0f, 0 };
   std::array<vk::ClearValue, 1> clearColours{clearDepthValue};
 
+  // Begin rendering using the framebuffer and renderpass
   vk::RenderPassBeginInfo shadowRpbi{};
   shadowRpbi.renderPass = *shadowRenderPass;
   shadowRpbi.framebuffer = *shadowFrameBuffer;
-  shadowRpbi.renderArea = vk::Rect2D{{0, 0}, {256, 256}};
+  shadowRpbi.renderArea = vk::Rect2D{{0, 0}, {shadowSize, shadowSize}};
   shadowRpbi.clearValueCount = (uint32_t)clearColours.size();
   shadowRpbi.pClearValues = clearColours.data();
 
@@ -258,6 +275,12 @@ int main() {
 
   update.update(device);
 
+  // Depth bias pulls the depth buffer value away from the surface to prevent artifacts.
+  // The bias changes as the triangle slopes away from the camera.
+  // Values taken from Sasha Willems' shadowmapping example.
+  const float depthBiasConstantFactor = 1.25f;
+  const float depthBiasSlopeFactor = 1.75f;
+
   // Set the static render commands for the main renderpass.
   window.setStaticCommands(
     [&](vk::CommandBuffer cb, int imageIndex, vk::RenderPassBeginInfo &rpbi) {
@@ -270,6 +293,7 @@ int main() {
       cb.bindVertexBuffers(0, vbo.buffer(), vk::DeviceSize(0));
       cb.bindIndexBuffer(ibo.buffer(), vk::DeviceSize(0), vk::IndexType::eUint32);
       cb.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, *pipelineLayout, 0, descriptorSets, nullptr);
+      cb.setDepthBias(depthBiasConstantFactor, 0.0f, depthBiasSlopeFactor);
       cb.drawIndexed(indexCount, 1, 0, 0, 0);
       cb.endRenderPass();
 
