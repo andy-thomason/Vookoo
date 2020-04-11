bd591ac60e228803e6504f8ee1706936029eaab3
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2020-04-11 08:52:45 -0600

    Partially handle resizing.

--- include/vku/vku_framework.hpp
+++ include/vku/vku_framework.hpp
@@ -247,6 +247,8 @@ public:
     //surface_ = vk::UniqueSurfaceKHR(surface);
     //surface_ = vk::UniqueSurfaceKHR(surface, vk::SurfaceKHRDeleter{ instance });
     surface_ = surface;
+    graphicsQueueFamilyIndex_ = graphicsQueueFamilyIndex;
+    physicalDevice_ = physicalDevice;
     instance_ = instance;
     device_ = device;
     presentQueueFamily_ = 0;
@@ -282,92 +284,17 @@ public:
       }
     }
 
-    auto surfaceCaps = pd.getSurfaceCapabilitiesKHR(surface_);
-    width_ = surfaceCaps.currentExtent.width;
-    height_ = surfaceCaps.currentExtent.height;
+    createSwapchain();
 
-    auto pms = pd.getSurfacePresentModesKHR(surface_);
-    vk::PresentModeKHR presentMode = pms[0];
-    if (std::find(pms.begin(), pms.end(), vk::PresentModeKHR::eFifo) != pms.end()) {
-      presentMode = vk::PresentModeKHR::eFifo;
-    } else {
-      std::cout << "No fifo mode available\n";
-      return;
-    }
+    createImages();
 
-    //std::cout << "using " << vk::to_string(presentMode) << "\n";
+    createDepthStencil();
 
-    vk::SwapchainCreateInfoKHR swapinfo{};
-    std::array<uint32_t, 2> queueFamilyIndices = { graphicsQueueFamilyIndex, presentQueueFamily_ };
-    bool sameQueues = queueFamilyIndices[0] == queueFamilyIndices[1];
-    vk::SharingMode sharingMode = !sameQueues ? vk::SharingMode::eConcurrent : vk::SharingMode::eExclusive;
-    swapinfo.imageExtent = surfaceCaps.currentExtent;
-    swapinfo.surface = surface_;
-    swapinfo.minImageCount = surfaceCaps.minImageCount + 1;
-    swapinfo.imageFormat = swapchainImageFormat_;
-    swapinfo.imageColorSpace = swapchainColorSpace_;
-    swapinfo.imageExtent = surfaceCaps.currentExtent;
-    swapinfo.imageArrayLayers = 1;
-    swapinfo.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
-    swapinfo.imageSharingMode = sharingMode;
-    swapinfo.queueFamilyIndexCount = !sameQueues ? 2 : 0;
-    swapinfo.pQueueFamilyIndices = queueFamilyIndices.data();
-    swapinfo.preTransform = surfaceCaps.currentTransform;;
-    swapinfo.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
-    swapinfo.presentMode = presentMode;
-    swapinfo.clipped = 1;
-    swapinfo.oldSwapchain = vk::SwapchainKHR{};
-    swapchain_ = device.createSwapchainKHRUnique(swapinfo);
+	  createRenderPass();
 
-    images_ = device.getSwapchainImagesKHR(*swapchain_);
-    for (auto &img : images_) {
-      vk::ImageViewCreateInfo ci{};
-      ci.image = img;
-      ci.viewType = vk::ImageViewType::e2D;
-      ci.format = swapchainImageFormat_;
-      ci.subresourceRange = {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1};
-      imageViews_.emplace_back(device.createImageView(ci));
-    }
+	  createFrameBuffers();
 
-    auto memprops = physicalDevice.getMemoryProperties();
-    depthStencilImage_ = vku::DepthStencilImage(device, memprops, width_, height_);
-
-    // Build the renderpass using two attachments, colour and depth/stencil.
-    vku::RenderpassMaker rpm;
-
-    // The only colour attachment.
-    rpm.attachmentBegin(swapchainImageFormat_);
-    rpm.attachmentLoadOp(vk::AttachmentLoadOp::eClear);
-    rpm.attachmentStoreOp(vk::AttachmentStoreOp::eStore);
-    rpm.attachmentFinalLayout(vk::ImageLayout::ePresentSrcKHR);
-
-    // The depth/stencil attachment.
-    rpm.attachmentBegin(depthStencilImage_.format());
-    rpm.attachmentLoadOp(vk::AttachmentLoadOp::eClear);
-    rpm.attachmentStencilLoadOp(vk::AttachmentLoadOp::eDontCare);
-    rpm.attachmentFinalLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
-
-    // A subpass to render using the above two attachments.
-    rpm.subpassBegin(vk::PipelineBindPoint::eGraphics);
-    rpm.subpassColorAttachment(vk::ImageLayout::eColorAttachmentOptimal, 0);
-    rpm.subpassDepthStencilAttachment(vk::ImageLayout::eDepthStencilAttachmentOptimal, 1);
-
-    // A dependency to reset the layout of both attachments.
-    rpm.dependencyBegin(VK_SUBPASS_EXTERNAL, 0);
-    rpm.dependencySrcStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput);
-    rpm.dependencyDstStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput);
-    rpm.dependencyDstAccessMask(vk::AccessFlagBits::eColorAttachmentRead|vk::AccessFlagBits::eColorAttachmentWrite);
-
-    // Use the maker object to construct the vulkan object
-    renderPass_ = rpm.createUnique(device);
-
-    for (int i = 0; i != imageViews_.size(); ++i) {
-      vk::ImageView attachments[2] = {imageViews_[i], depthStencilImage_.imageView()};
-      vk::FramebufferCreateInfo fbci{{}, *renderPass_, 2, attachments, width_, height_, 1 };
-      framebuffers_.push_back(device.createFramebufferUnique(fbci));
-    }
-
-    vk::SemaphoreCreateInfo sci;
+	  vk::SemaphoreCreateInfo sci;
     imageAcquireSemaphore_ = device.createSemaphoreUnique(sci);
     commandCompleteSemaphore_ = device.createSemaphoreUnique(sci);
     dynamicSemaphore_ = device.createSemaphoreUnique(sci);
@@ -399,7 +326,7 @@ public:
     ok_ = true;
   }
 
-  /// Dump the capabilities of the physical device used by this window.
+	/// Dump the capabilities of the physical device used by this window.
   void dumpCaps(std::ostream &os, vk::PhysicalDevice pd) const {
     os << "Surface formats\n";
     auto fmts = pd.getSurfaceFormatsKHR(surface_);
@@ -505,7 +432,12 @@ public:
     presentInfo.pImageIndices = &imageIndex;
     presentInfo.waitSemaphoreCount = 1;
     presentInfo.pWaitSemaphores = &ccSema;
-    presentQueue().presentKHR(presentInfo);
+    try {
+	    presentQueue().presentKHR(presentInfo);
+    } catch (const vk::OutOfDateKHRError &e) {
+    	std::cerr << "Recreating things." << std::endl;
+    	recreate();
+    }
   }
 
   /// Return the queue family index used to present the surface to the display.
@@ -575,8 +507,133 @@ public:
   /// Return the number of swap chain images.
   int numImageIndices() const { return (int)images_.size(); }
 
+  /// Create a new swapchain and destroy the previous one if any.
+  void createSwapchain() {
+    auto pms = physicalDevice_.getSurfacePresentModesKHR(surface_);
+    vk::PresentModeKHR presentMode = pms[0];
+    if (std::find(pms.begin(), pms.end(), vk::PresentModeKHR::eFifo) !=
+        pms.end()) {
+      presentMode = vk::PresentModeKHR::eFifo;
+    } else {
+      std::cout << "No fifo mode available\n";
+      return;
+    }
+
+    auto surfaceCaps = physicalDevice_.getSurfaceCapabilitiesKHR(surface_);
+    width_ = surfaceCaps.currentExtent.width;
+    height_ = surfaceCaps.currentExtent.height;
+    vk::SwapchainCreateInfoKHR swapinfo{};
+    std::array<uint32_t, 2> queueFamilyIndices = {graphicsQueueFamilyIndex_,
+                                                  presentQueueFamily_};
+    bool sameQueues = queueFamilyIndices[0] == queueFamilyIndices[1];
+    vk::SharingMode sharingMode = !sameQueues ? vk::SharingMode::eConcurrent
+                                              : vk::SharingMode::eExclusive;
+    swapinfo.imageExtent = surfaceCaps.currentExtent;
+    swapinfo.surface = surface_;
+    swapinfo.minImageCount = surfaceCaps.minImageCount + 1;
+    swapinfo.imageFormat = swapchainImageFormat_;
+    swapinfo.imageColorSpace = swapchainColorSpace_;
+    swapinfo.imageExtent = surfaceCaps.currentExtent;
+    swapinfo.imageArrayLayers = 1;
+    swapinfo.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
+    swapinfo.imageSharingMode = sharingMode;
+    swapinfo.queueFamilyIndexCount = !sameQueues ? 2 : 0;
+    swapinfo.pQueueFamilyIndices = queueFamilyIndices.data();
+    swapinfo.preTransform = surfaceCaps.currentTransform;
+    ;
+    swapinfo.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
+    swapinfo.presentMode = presentMode;
+    swapinfo.clipped = 1;
+    swapinfo.oldSwapchain = vk::SwapchainKHR{};
+    swapchain_ = device_.createSwapchainKHRUnique(swapinfo);
+  }
+
+  void createImages() {
+    images_ = device_.getSwapchainImagesKHR(*swapchain_);
+    for (auto &iv : imageViews_) {
+      device_.destroyImageView(iv);
+    }
+    imageViews_.clear();
+    for (auto &img : images_) {
+      vk::ImageViewCreateInfo ci{};
+      ci.image = img;
+      ci.viewType = vk::ImageViewType::e2D;
+      ci.format = swapchainImageFormat_;
+      ci.subresourceRange = {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1};
+      imageViews_.emplace_back(device_.createImageView(ci));
+    }
+  }
+
+  void createFrameBuffers() {
+    framebuffers_.clear();
+    for (int i = 0; i != imageViews_.size(); ++i) {
+      vk::ImageView attachments[2] = {imageViews_[i],
+                                      depthStencilImage_.imageView()};
+      vk::FramebufferCreateInfo fbci{{},     *renderPass_, 2, attachments,
+                                     width_, height_,      1};
+      framebuffers_.push_back(device_.createFramebufferUnique(fbci));
+    }
+  }
+
+  void createDepthStencil() {
+    auto memprops = physicalDevice_.getMemoryProperties();
+    depthStencilImage_ =
+        vku::DepthStencilImage(device_, memprops, width_, height_);
+  }
+
+  void createRenderPass() { // Build the renderpass using two attachments,
+                            // colour and depth/stencil.
+    RenderpassMaker rpm;
+
+    // The only colour attachment.
+    rpm.attachmentBegin(swapchainImageFormat_);
+    rpm.attachmentLoadOp(vk::AttachmentLoadOp::eClear);
+    rpm.attachmentStoreOp(vk::AttachmentStoreOp::eStore);
+    rpm.attachmentFinalLayout(vk::ImageLayout::ePresentSrcKHR);
+
+    // The depth/stencil attachment.
+    rpm.attachmentBegin(depthStencilImage_.format());
+    rpm.attachmentLoadOp(vk::AttachmentLoadOp::eClear);
+    rpm.attachmentStencilLoadOp(vk::AttachmentLoadOp::eDontCare);
+    rpm.attachmentFinalLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
+
+    // A subpass to render using the above two attachments.
+    rpm.subpassBegin(vk::PipelineBindPoint::eGraphics);
+    rpm.subpassColorAttachment(vk::ImageLayout::eColorAttachmentOptimal, 0);
+    rpm.subpassDepthStencilAttachment(
+        vk::ImageLayout::eDepthStencilAttachmentOptimal, 1);
+
+    // A dependency to reset the layout of both attachments.
+    rpm.dependencyBegin(VK_SUBPASS_EXTERNAL, 0);
+    rpm.dependencySrcStageMask(
+        vk::PipelineStageFlagBits::eColorAttachmentOutput);
+    rpm.dependencyDstStageMask(
+        vk::PipelineStageFlagBits::eColorAttachmentOutput);
+    rpm.dependencyDstAccessMask(vk::AccessFlagBits::eColorAttachmentRead |
+                                vk::AccessFlagBits::eColorAttachmentWrite);
+
+    // Use the maker object to construct the vulkan object
+    renderPass_ = rpm.createUnique(device_);
+  }
+
+  void recreate() {
+    device_.waitForFences(commandBufferFences_, VK_TRUE,
+                          std::numeric_limits<uint64_t>::max());
+    createSwapchain();
+
+    createImages();
+
+    createDepthStencil();
+
+    createFrameBuffers();
+  }
+
+  vk::Device device() const { return device_; }
+
 private:
   vk::Instance instance_;
+  vk::PhysicalDevice physicalDevice_;
+  uint32_t graphicsQueueFamilyIndex_;
   vk::SurfaceKHR surface_;
   vk::UniqueSwapchainKHR swapchain_;
   vk::UniqueRenderPass renderPass_;
